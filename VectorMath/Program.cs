4acc7072b260c102ecefbfad29d1da8b19e1b66f
will12 <will12@F1092-30-2118>
2019-12-09 16:15:55 -0600

    Class participation

--- VectorMath/Program.cs
+++ VectorMath/Program.cs
@@ -7,6 +7,21 @@ namespace VectorMath
         static void Main(string[] args)
         {
             Console.WriteLine("Fun with vectors");
+            Vector3 one = new Vector3(3, 4, 5);
+            Vector3 two = new Vector3(4, 8, 0);
+
+            Console.WriteLine($"Operator +: {one + two} ");
+
+            Console.WriteLine($"Subtraction -: {one - two}");
+
+
+            Console.WriteLine($"Scalar multiplication: {two * 2}");
+
+
+            Console.WriteLine($"Should not be equal: {one == two}");
+
+
+            Console.WriteLine($"Increment: {one++}");
         }
     }
 }
--- VectorMath/Vector3.cs
+++ VectorMath/Vector3.cs
@@ -10,6 +10,11 @@ namespace VectorMath
         double Y;
         double Z;
 
+        public override string ToString()
+        {
+            return $"<{this.X}, {this.Y}, {this.Z}>";
+        }
+
         public Vector3(double x, double y, double z)
         {
             X = x;
@@ -18,11 +23,51 @@ namespace VectorMath
         }
 
         // Vector Addition
+        public static Vector3 Add(Vector3 a, Vector3 b)
+        {
+            return new Vector3(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+        }
 
+        public Vector3 add(Vector3 other)
+        {
+            return new Vector3(this.X + other.X, this.Y + other.Y, this.Z + other.Z);
+        }
+
+        public void AddInPlace(Vector3 other)
+        {
+            this.X += other.X;
+            this.Y += other.Y;
+            this.Z += other.Z;
+        }
+
+        public static Vector3 operator +(Vector3 a, Vector3 b)
+        {
+            return new Vector3(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+        }
         // Vector Subtraction 
-
+        public static Vector3 operator -(Vector3 a, Vector3 b)
+        {
+            return new Vector3(a.X - b.X, a.Y - b.Y, a.Z - b.Z);
+        }
         // Vector Scaling 
-
+        public static Vector3 operator *(Vector3 b, double a)
+        {
+            return new Vector3(a * b.X, a * b.Y, a * b.Z);
+        }
         // Vector Comparison
+        public static bool operator ==(Vector3 a, Vector3 b)
+        {
+            return a.X == b.X && a.Y == b.Y && a.Z == b.Z;
+        }
+        public static bool operator !=(Vector3 a, Vector3 b)
+        {
+            return a.X != b.X || a.Y != b.Y || a.Z != b.Z;
+        }
+
+        public static Vector3 operator ++(Vector3 a)
+        {
+           return new Vector3(a.X++, a.Y++, a.Z++);
+        }
+
     }
 }
